805f6463f07550c522bb450fc0741d56cccb00b3
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-06-13 14:59:46 +0100

    feat:Implemented the redis db connection

---
+++ src/infrastructure/databases/redis/RedisDBManager.ts
@@ -0,0 +1,41 @@
+import {Logger} from 'winston';
+import { createClient, RedisClientType } from 'redis';
+import { Config } from 'convict';
+import config from '../../../config/index';
+import {logger} from '../../logger/index'
+
+
+/**
+ * Manages connection to RedisDBManager
+ */
+
+class RedisDBManager {
+    private readonly config: Config<any>;
+    private readonly port: number;
+    private readonly host: string;
+    private readonly log: Logger;
+
+    constructor() {
+        this.config = config;
+        this.port = config.get('cache.cachePort');
+        this.host = config.get('cache.cacheHost');
+        this.log = logger;
+    }
+
+
+    async connect() {
+        // const portRedis = process.env.PORT_REDIS || '6379';
+        const redisClient = createClient();
+        redisClient.on('error', (err) => this.log.info('Redis Client Error', err));
+        await redisClient.connect();
+        redisClient.on('connect',  (err) => {
+            this.log.info('Connected to Redis');
+        })
+    }
+
+    async consume (redisClient: RedisClientType) {
+        
+    }
+}
+
+export default RedisDBManager;
