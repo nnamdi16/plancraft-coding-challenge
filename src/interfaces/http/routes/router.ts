e0726a7576fa6cac7aa9f7dd5c815674eb64bd63
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-06-14 15:15:12 +0100

    rempved unused lines of code

--- src/interfaces/http/routes/router.ts
+++ src/interfaces/http/routes/router.ts
@@ -4,7 +4,6 @@ import cors from "cors";
 import errorHandler from "../../../infrastructure/middlewares/errorHandler";
 import helmet from "helmet";
 import morgan from "morgan";
-import v1Routes from "./v1";
 import config from './../../../config/index'
 import { logger, morganOption } from "../../../infrastructure/logger/index";
 import projectRoutes from '@interfaces/http/routes/v1/project';
@@ -14,7 +13,6 @@ import projectRoutes from '@interfaces/http/routes/v1/project';
  */
 export default () => {
   const router = Router();
-  // const app = express();
 
   logger.info('We are routing at the moment')
 router.use(helmet());
@@ -56,53 +54,9 @@ router.use(express.urlencoded({ extended: false, limit: bodyLimit }));
     })
   });
   
-  router.use("/v1", projectRoutes);
-
-//   router.use(error404);
+  router.use("/v1/project", projectRoutes);
 
   router.use(errorHandler);
 
   return router;
 };
-
-// const router = express.Router();
-// router.use(helmet());
-// const NODE_ENV = config.get("app.env");
-// router.use(morgan('combined', NODE_ENV === "production" ? "combined" : morganOption));
-// const bodyLimit = config.get("app.bodyLimit");
-// router.use(
-//   express.json({
-//     limit: bodyLimit,
-//   }),
-// );
-// router.use(express.urlencoded({ extended: false, limit: bodyLimit }));
-
-
-//   // Setup CORS
-//   const allowedOrigins = config.get("app.allowedOrigins");
-//   router.use(
-//     cors({
-//       origin: (origin, cb) => {
-//         if (allowedOrigins.trim() === "*") {
-//           cb(null, true);
-//         } else {
-//           const origins = allowedOrigins.split(",");
-//           console.log(origins)
-//           if (origins.indexOf(origin as string) !== -1 || !origin) {
-//             cb(null, true);
-//           } else {
-//             cb(new Error(`Origin('${origin}') not allowed`));
-//           }
-//         }
-//       },
-//       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-//     }),
-//   );
-// router.get("/", (req, res) => {
-//   console.log('I am here to look for you');
-//   return res.json({
-//     message: "Sample API template using Clean Architecture",
-//   })
-// });
-
-// export default router;
