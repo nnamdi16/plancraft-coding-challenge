6f5a3b53ced13ee3941e6da28aeac20d51a710a2
nnamdi16 <nwabuokeinnamdi@gmail.com>
2022-06-14 15:15:36 +0100

    feat: refactored the route

--- src/interfaces/http/routes/v1/project.ts
+++ src/interfaces/http/routes/v1/project.ts
@@ -5,11 +5,11 @@ import { Router } from "express";
 const router = Router();
 const api = new ProjectController();
 
-router.route('/:id').get(api.retrieve);
-router.route('/').get(api.retrieveAll);
+router.route('/:name/:id').get(api.retrieve);
+router.route('/:name').get(api.retrieveAll);
 router.route('/').post(api.create);
 router.route('/').delete(api.deleteProjects);
-router.route('/:id').delete(api.deleteProject);
-router.route('/:id').put(api.updateProject);
+router.route('/:name/:id').delete(api.deleteProject);
+router.route('/update/:id').put(api.updateProject);
 
 export default router;
